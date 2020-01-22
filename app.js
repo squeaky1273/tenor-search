34709995d9584171634ef55033d5f54d4dee799d
squeaky1273 <pjriddell22@gmail.com>
2020-01-22 15:35:42 -0800

    added search form

--- app.js
+++ app.js
@@ -12,10 +12,8 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
+    console.log(req.query) // => "{ term: hey" }
+    res.render('home')
   })
   app.get('/greetings/:name', (req, res) => {
     // grab the name from the path provided
@@ -23,9 +21,15 @@ app.get('/', (req, res) => {
     // render the greetings view, passing along the name
     res.render('greetings', { name });
   })
-  
+
 // Start Server
 
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
+// example URL "http://localhost:3000/?term=hey"
+app.get('/', (req, res) => {
+    console.log(req.query) // => "{ term: hey" }
+  
+    res.render('home')
+  })
